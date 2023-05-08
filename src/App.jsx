d1c4a1db33fc5a854d1e0c5ee2d626594979f003
vaneortiz21 <vanessa.ortizc@uanl.edu.mx>
2023-05-08 17:32:32 -0600

    Pagina Base front

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Inicio />
       <Cursos />
       <Eventos />
-      <Usuario />
+      
       
     </div>
   )
--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-scroll';
 import styles from './Navbar.module.css';
 import {AiOutlineMenu} from "react-icons/ai";
 import {AiOutlineClose} from "react-icons/ai";
+import { useScrollPosition } from '../Hooks/scroolPosition';
 const Navbar = () => {
     const [NavbarOpen, setNavbarOpen]= useState(false);
     const[windowDimension, setWindowDimension]=useState({
@@ -40,25 +41,33 @@ const Navbar = () => {
         link:"Eventos",
     },
    
-    {
-        id:4,
-        link:"Usuario",
-    },
+    
   ];
   
+    const scrollPosition= useScrollPosition();
     return (
-    <div className={!NavbarOpen ? styles.Navbar : styles.NavOpen}>
+    <div 
+        className={
+            NavbarOpen 
+            ? styles.NavOpen 
+            : scrollPosition>0
+            ? styles.navOnScroll
+            : styles.Navbar
+        }
+    >
         {!NavbarOpen && <p className={styles.logo}>Wujuballons</p>}
         {!NavbarOpen && windowDimension.width <800 ? (
-        <AiOutlineMenu onClick={() => setNavbarOpen(!NavbarOpen)} size={25} />
-         ): windowDimension.width <800 &&(
+        <AiOutlineMenu  onClick={() => setNavbarOpen(!NavbarOpen)} size={25} />
+         ): (windowDimension.width <800 &&(
          <AiOutlineClose 
          onClick={() => setNavbarOpen(!NavbarOpen)} 
          color='#ffffff'
-         size={25}/>
+         size={25}
+         />
+         )
         )}
         { NavbarOpen && (
-            <ul>
+            <ul className={styles.linksContainer}>
             {links.map((x) =>(
                 <div>
                     <Link 
@@ -68,11 +77,37 @@ const Navbar = () => {
                     duration={500}
                     className={styles.navLink}
                     >
-                        {x.link==="Eventos" ? "Eventos": x.link}</Link>
+                        {x.link==="Eventos" ? "Eventos": x.link}
+                        </Link>
                         <div className={styles.border}></div>
                 </div>
 
             ))}
+        </ul>
+        )}
+        {
+            windowDimension.width > 800 && (
+            <ul className={styles.linksContainer}>
+            {links.map((x) =>(
+                <div>
+                    <Link 
+                    onClick={() => setNavbarOpen(false)}
+                    to={x.link}
+                    smooth
+                    duration={500}
+                    className={styles.navLink}
+                    >
+                        {x.link==="Eventos" ? "Eventos": x.link}
+                        </Link>
+                        <div className={styles.border}></div>
+                </div>
+
+            ))}
+            <Link onClick={() => setNavbarOpen(false)}
+                    to="Usuario"
+                    smooth
+                    duration={500}
+                    className={styles.usuarioLink}>Usuario</Link>
         </ul>
         )}
     </div>

9a1b830088efb576cd35c71823e1b822715642d7
danaehdz <danae56@hotmail.com>
2023-05-14 17:45:18 -0600

    Se agregaron pasos de la Inscripción en cursos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 
 import  Navbar from "./Menu/Navbar";
 import Inicio from "./Pantallas/Inicio";
@@ -5,12 +7,15 @@ import Cursos from "./Pantallas/Cursos";
 import Eventos from "./Pantallas/Eventos";
 import Usuario from "./Pantallas/Usuario";
 import Slider1 from "./Pantallas/Slider1";
+import Steps from "./Pantallas/Steps";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -23,9 +28,11 @@ function App() {
         <Route exact path="/Eventos" element={<Eventos/>}/>
         <Route exact path="/Usuario" element={<Usuario/>}/>
         <Route exact path="/Slider1" element={<Slider1/>}/>
+        <Route exact path="/Inscripción" element={<Steps/>}/>
        </Routes>
       </BrowserRouter>
-    
+
+
     </div>
   )
 }

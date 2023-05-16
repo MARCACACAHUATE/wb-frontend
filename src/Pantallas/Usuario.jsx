eda332bfc02f9277f658ff2cec6b7e89cecd4528
marcela-olguin <89671402+marcela-olguin@users.noreply.github.com>
2023-05-15 20:00:11 -0600

    Update Usuario.jsx

--- src/Pantallas/Usuario.jsx
+++ src/Pantallas/Usuario.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Usuario = () => {
   return (
-    <div>Usuario</div>
+    <div className='style.Usuario'>
+      <form action="">
+        <div> 
+          <label htmlFor="coment">Tipo de Usuario</label>
+        <select name="Usuario" id="">
+          <option value="op1">Administrador</option>
+          <option value="op2">Empleado</option>
+        </select>
+        </div>
+        
+        <label htmlFor="email">Correo Electronico</label>
+        <input type="email" id='email' name='email' placeholder='Ingresa tu correo' required />
+
+        <label htmlFor="password">ContraseÃ±a</label>
+        <input type="password" id='password' name='password' placeholder='Ingresa tu contraseÃ±a' required />
+
+        <button type='submit'>Ingresar</button>
+      </form>
+
+    </div>
+
   )
 }
 

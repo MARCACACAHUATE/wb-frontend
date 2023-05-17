b8735156b27ccb0cadc0106c2535a13c057c0f7b
marcela-olguin <marcela.olguinlz@uanl.edu.mx>
2023-05-16 23:33:48 -0600

    Update Usuario.jsx

--- src/Pantallas/Usuario.jsx
+++ src/Pantallas/Usuario.jsx
@@ -3,27 +3,39 @@ import styles from './Usuario.module.css';
 
 const Usuario = () => {
   return (
+    <body>
     <div className='style.Usuario'>
       <form action="">
-        <div> 
-          <label htmlFor="coment">Tipo de cuenta</label>
-        <select name="Usuario" id="">
-          <option value="op1">Administrador</option>
-          <option value="op2">Empleado</option>
-        </select>
+        <h2>Ingresa a tu cuenta</h2>
+        <span className='line'></span>
+
+        <div className='input-group'>
+            <div className='form-group form-cont'>
+              <label htmlFor="coment">Tipo de Usuario</label>
+              <select name="form-control" id="">
+                <option value="Op1">Adminsitrador</option>
+                <option value="Op2">Empleado</option>
+              </select>
+            </div>
+          
+          <label htmlFor="email">Email</label>
+          <input type="email" name='email' placeholder='Ingresa tu email' id='email' />
+
+          <label htmlFor="contraseña">Contraseña</label>
+          <input type="password" name="password" placeholder='Ingresa tu contraseña' id="contraseña" />
+
+          <div className='form-txt'>
+              <a href="#">Politica de privacidad</a>
+              <a href="#">Terminos y condiciones</a>
+          </div>
+
+          <input className='btn' type="submit" value={Ingresar}/>
+
         </div>
-        
-        <label htmlFor="email">Correo Electronico</label>
-        <input type="email" id='email' name='email' placeholder='Ingresa tu correo' required />
 
-        <label htmlFor="password">Contraseña</label>
-        <input type="password" id='password' name='password' placeholder='Ingresa tu contraseña' required />
-
-        <button type='submit'>Ingresar</button>
       </form>
-
     </div>
-
+    </body>
   )
 }
 

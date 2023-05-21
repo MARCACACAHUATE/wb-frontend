add9300396f9af581dcc24bf7278cab0688cdbf2
vaneortiz21 <vanessa.ortizc@uanl.edu.mx>
2023-05-21 03:01:09 -0600

    correccion error footer y pagina eventos

--- src/Pantallas/Eventos.jsx
+++ src/Pantallas/Eventos.jsx
@@ -7,14 +7,15 @@ import data from "./DatosEventos.js"
 const Eventos = () => {
  
   return (
-    <>
+    <div className="even">
     <h1 className='titulo'>Eventos/Servicios</h1>
-    <ul>
+    <ul className='cuadros'>
     {data.map(paquete =>(<ButtonsE key ={paquete.id} paquete ={paquete}/> )
     )}
     </ul>
-    </>
-    );
+    </div>
+    
+    )
     
 };
 

ec6ddf1eeb9f00b594829e10a63c9ed63bb056c9
Dante Portales <dantport28@gmail.com>
2023-05-21 14:57:00 -0600

    eventos conectado al backend y ajustes de diseño

--- src/Componentes/BotonesEventos.jsx
+++ src/Componentes/BotonesEventos.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { formatCurrency } from './../Hooks/currencyUtils';
+import c1 from "../img/CursoGuadalajara.jpg";
 
 
 const ButtonsE =({paquete})=>{
     return  (
      <section className="botonesE">
        <ul className='list'>
-         <img src={paquete.imageUrl} alt={paquete.decoration} width={100} />
-         <li className='deco'><b> Decoración:</b> {paquete.decoration}</li>
+        <div className='img-container'>
+          <img src={c1} alt={paquete.nombrePaquete} width={100} />
+        </div>
+         <li className='deco'>{paquete.nombrePaquete} <br /><strong>{paquete.estado}</strong></li>
          <li className='include'><b>Incluye: </b> <ul>
-               {paquete.includes.map((item, index) => (
-                 <li key={index}>{item}</li>
-               ))}
+               {/* {paquete.includes.map((item, index) => ( */}
+                 <li>{paquete.ocasion}</li>
+                 <li>{paquete.servicios}</li>
+                 <li>{paquete.mobiliario}</li>
+                 <li>Reservacion: {formatCurrency(paquete.costo_reservacion)}</li>
+                 <li>Envio: {formatCurrency(paquete.costoEnvioMaterial)}</li>
+                 <li>Total: <strong>{formatCurrency(paquete.costo_total)}</strong></li>
+               {/* ))} */}
              </ul> </li>
          <li > 
          <div className='customButton'>
--- src/Pantallas/Eventos.jsx
+++ src/Pantallas/Eventos.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Eventos.css'
 import ButtonsE from "../Componentes/BotonesEventos.jsx"
 import data from "./DatosEventos.js"
+import { reqqResapi } from "../api/reqRes";
+import styles from "./../Pantallas/Calendario.module.css";
 
 
 const Eventos = () => {
  
+  const [isUpdate, setisUpdate] = useState(false);
+  const [EventosList, setEventosList] = useState([]);
+
+  useEffect(() => {
+    getEventosList();
+  }, [isUpdate]);
+
+  const getEventosList = async() => {
+
+    const res = await reqqResapi.get('api/eventos').then(res => {
+
+        if (res.data.error) {
+              console.log(res.data.error);
+        } else {
+              res.data.data.sort((a, b) => new Date(b.id) - new Date(a.id));
+              setEventosList(res.data.data);
+              console.log(res.data.data);
+        }
+
+    });   
+  }
+
   return (
     <div className="even">
-    <h1 className='titulo'>Eventos/Servicios</h1>
+      <div className={styles.cont1}>
+        <br /><br />
+        <h1 className={styles.titulo}>
+        ¡Da un vistazo a nuestros paquetes de Eventos!
+        </h1>
+        <br /><br />
+      </div>
     <ul className='cuadros'>
-    {data.map(paquete =>(<ButtonsE key ={paquete.id} paquete ={paquete}/> )
+    {EventosList.map(paquete =>(<ButtonsE key ={paquete.id} paquete ={paquete}/> )
     )}
     </ul>
     </div>

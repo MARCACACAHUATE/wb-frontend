d5be388a128a98a7a51b8d42d002243873230448
Diegstd <diego_std@hotmail.com>
2023-05-22 03:32:16 -0600

    Modificacion FirstStep, separacioncurso

--- src/Pantallas/FirstStep.jsx
+++ src/Pantallas/FirstStep.jsx
@@ -26,24 +26,48 @@ const FirstStep = () => {
     fetchCursos();
   }, []);
 
+  const handleCursoSelection = (event) => {
+    const cursoId = event.target.value;
+    setUserData({ ...userData, "IdCursos": cursoId });
+  };
+
+  const saveUserData = async () => {
+    try {
+      const updatedUserData = {
+        ...userData,
+        Cantidad_personas_contratadas: parseInt(userData.Cantidad_personas_contratadas),
+        Edad: parseInt(userData.Edad),
+        IdCursos: parseInt(userData.IdCursos),
+        Telefono: parseInt(userData.Telefono),
+      };
+      console.log(updatedUserData);
+      const response = await reqqResapi.post('api/CursoSeparacion', updatedUserData);
+      console.log(response.data);
+      // Cualquier acción adicional después de guardar los datos en la base de datos
+      setStep(2); // Pasar al siguiente paso
+    } catch (error) {
+      console.error("Error saving user data:", error);
+    }
+  };
+
   return (
     <div className="conteiner">
       <div>
-        <TextField label="Nombre" value={userData['nombre']} onChange={(e) => setUserData({ ...userData, "nombre": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Nombre" value={userData['First_name']} onChange={(e) => setUserData({ ...userData, "First_name": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
       <div>
-        <TextField label="Apellidos" value={userData['apellidos']} onChange={(e) => setUserData({ ...userData, "apellidos": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Apellidos" value={userData['Last_name']} onChange={(e) => setUserData({ ...userData, "Last_name": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
       <div>
-        <TextField label="Edad" value={userData['edad']} onChange={(e) => setUserData({ ...userData, "edad": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Edad" value={userData['Edad']} onChange={(e) => setUserData({ ...userData, "Edad": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
       <div>
-        <TextField label="Teléfono" value={userData['telefono']} onChange={(e) => setUserData({ ...userData, "telefono": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Teléfono" value={userData['Telefono']} onChange={(e) => setUserData({ ...userData, "Telefono": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
       <div>
-        <TextField label="Correo electrónico" value={userData['correo']} onChange={(e) => setUserData({ ...userData, "correo": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Correo electrónico" value={userData['Email']} onChange={(e) => setUserData({ ...userData, "Email": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
-      <Form.Select aria-label="Default select example">
+      <Form.Select aria-label="Default select example" onChange={handleCursoSelection}>
         <option>Seleccione el curso al que desea inscribirse</option>
         {cursos.map((curso) => (
           <option key={curso.idCursos} value={curso.idCursos}>
@@ -52,20 +76,20 @@ const FirstStep = () => {
         ))}
       </Form.Select>
       <div>
-        <TextField label="Total de personas invitadas" value={userData['personas']} onChange={(e) => setUserData({ ...userData, "personas": e.target.value })} margin="normal" variant="outlined" color="secondary" />
+        <TextField label="Total de personas invitadas" value={userData['Cantidad_personas_contratadas']} onChange={(e) => setUserData({ ...userData, "Cantidad_personas_contratadas": e.target.value })} margin="normal" variant="outlined" color="secondary" />
       </div>
       <div>
-        <Form.Select aria-label="Default select example" value={userData['pagoCompleto']} onChange={(e) => setUserData({ ...userData, "pagoCompleto": e.target.value })}>
+        <Form.Select aria-label="Default select example" value={userData['TipoPago']} onChange={(e) => setUserData({ ...userData, "TipoPago": e.target.value === "true" })}>
           <option>Seleccione el tipo de pago</option>
-          <option value={true}>Pago Completo</option>
-          <option value={false}>Pago de Reservación</option>
+          <option value="true">Pago Completo</option>
+          <option value="false">Pago de Reservación</option>
         </Form.Select>
       </div>
       <div>
-        <Button variant="contained" onClick={() => setStep(2)} color="primary">Next</Button>
+        <Button variant="contained" onClick={saveUserData} color="primary">Next</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FirstStep;
